b7993b00878c3489470d7a2c2fe09e2074ebe3fe
NotANewt <meegafauna@gmail.com>
2022-04-22 19:42:28 -0500

    added routes for reactionRoutes and thoughtRoutes into the index.js in the routes api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const reactionRoutes = require("./reactionRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 
 router.use("/users", userRoutes);
+router.use("/reactions", reactionRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
-
-// older routes from miniproj
-
-const courseRoutes = require("./courseRoutes");
-const studentRoutes = require("./studentRoutes");
-
-router.use("/courses", courseRoutes);
-router.use("/students", studentRoutes);
